4cc62283d87dc71c161da96a763d6920930538c9
Emiwiana <a51635@alunos.isel.pt>
2026-05-03 00:13:28 +0100

    Add weather API client and data models
    Introduce WeatherApiClient using Ktor (Android engine) with ContentNegotiation/JSON configured (prettyPrint, isLenient, ignoreUnknownKeys). Adds getWeather(lat, lon) to fetch current and hourly data from open-meteo API. Add serializable data models: WeatherData, CurrentWeather, Hourly, and WMO_WeatherCode enum with a helper getWeatherCodeMap to map codes to enum values.

---
+++ TP3/android/app/src/main/java/com/example/cooljettpackweatherapp/data/WeatherApiClient.kt
@@ -0,0 +1,38 @@
+package com.example.cooljettpackweatherapp.data
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object WeatherApiClient {
+    private val client = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    suspend fun getWeather(lat: Float, lon: Float): WeatherData? {
+        val reqString = buildString {
+            append("https://api.open-meteo.com/v1/forecast?")
+            append("latitude=${lat}&longitude=${lon}&")
+            append("current_weather=true&")
+            append("hourly=temperature_2m,weathercode,pressure_msl,windspeed_10m")
+        }
+        println("Getting URL: $reqString")
+
+        return try {
+            client.get(reqString).body<WeatherData>()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
